b3e4908222240769f9e2c7520f3f6280417f813d
PraiseImmanuel <praiseimmanuel601@gmail.com>
2022-07-30 03:00:58 +0100

    Update File

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Divider = () => {
     return(
-        <picture style={{ padding: '2.5rem 0 1rem'}}>
+        <picture style={{ padding: '2.25rem 0 1rem'}}>
             <source media="(min-width:465px)" srcSet="/images/pattern-divider-mobile.svg" style={{width: '100%'}} />
             <img src="/images/pattern-divider-desktop.svg" alt="divider" style={{width: '100%'}} />
         </picture>
